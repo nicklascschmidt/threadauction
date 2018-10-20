926056b73dd42d2b40f6ab728e75d3a8f49ea47f
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2018-10-20 14:06:56 -0700

    validation, bootstrap clean

--- client/src/pages/signup/userValidation.js
+++ client/src/pages/signup/userValidation.js
@@ -5,18 +5,21 @@ export const validateInputs = (data) => {
 
   let errorArray = [];
 
+  // USERNAME VALIDATION
   if (data.username.length > 20 || data.username.length < 3) {
     errorArray.push('Username must be between 3-20 characters.');
   } else {
     errorArray.push('Username looks good!');
   }
 
-  if (data.password.length > 20 || data.password.length < 6) {
-    errorArray.push('Password must be between 6-20 characters.');
+  // PASSWORD VALIDATION
+  if (data.password.length > 20 || data.password.length < 8) {
+    errorArray.push('Password must be between 8-20 characters.');
   } else {
     errorArray.push('Password looks good!');
   }
 
+  // EMAIL VALIDATION
   function checkEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -28,21 +31,24 @@ export const validateInputs = (data) => {
   } else {
     errorArray.push('Email looks good!');
   }
-  
-  // if (data.password.length > 20 || data.password.length < 6) {
-  //   errorArray.push('Password must be between 6-20 characters.');
-  // } else {
-  //   errorArray.push('Password looks good!');
-  // }
 
+  // STATE VALIDATION
   const stateArray = ['AL',	'AK',	'AZ',	'AR',	'CA',	'CO',	'CT',	'DE',	'DC',	'FL',	'GA',	'HI',	'ID',	'IL',	'IN',	'IA',	'KS',	'KY',	'LA',	'ME',	'MD',	'MA',	'MI',	'MN',	'MS',	'WY',	'MO',	'MT',	'NE',	'NV',	'NH',	'NJ',	'NM',	'NY',	'NC',	'ND',	'OH',	'OK',	'OR',	'PA',	'RI',	'SC',	'SD',	'TN',	'TX',	'UT',	'VT',	'VA',	'WA',	'WV',	'WI'];
   const stateExists = stateArray.includes(data.stateUSA.toUpperCase());
   console.log('stateExists',stateExists);
   console.log('data.stateUSA',data.stateUSA);
-  if (stateExists) {
-    errorArray.push('State looks good!.');
-  } else {
+  if (!stateExists) {
     errorArray.push('Please enter a valid 2-letter state code.');
+  } else {
+    errorArray.push('State looks good!');
+  }
+
+  // ZIP VALIDATION
+  let zipType = typeof data.zip;
+  if (zipType === 'number' && data.zip.length === 5) {
+    errorArray.push('Zip looks good!');
+  } else {
+    errorArray.push('Please enter a valid 5-number zip code.');
   }
 
 
