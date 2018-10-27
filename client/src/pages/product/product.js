404626c91b060af0c8b66caaa1ff3f68deec8876
TamilaTE <tamilaaghdami@yahoo.com>
2018-10-27 10:34:10 -0700

    product page started

---
+++ client/src/pages/product/product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Product extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state({
+
+        })
+    }
+
+    // 
+    componentDidMount = () => {
+        // show product title, descrip, image, etc.
+        // axios
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Sup</h3>
+            </div>
+        )
+    }
+}
