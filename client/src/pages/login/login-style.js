d14b5429ed7e7832e8241f71bd667dca1a50b96e
devprodark <devprodark@gmail.com>
2018-11-05 20:22:20 -0800

    some more styling done

--- client/src/pages/login/login-style.js
+++ client/src/pages/login/login-style.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components';
 
-const Wrapper = styled('section')`
-  
+const Wrapper = styled('fragment')`
+display: flex;
+margin-right: 30%;
+margin-left: 30%;
+background-color: rgb(232, 234,237);
+justify-content: center;
+padding: 50px;
+box-shadow: 15px 10px 10px gray;
+  form {
+    input{
+      width: 100%;
+      margin-bottom: 30px;
+    }
+    button {
+      font-size: 1rem;
+      border-radius: 5px;
+    }
+    button:hover {
+      background-color: rgb(99, 180,245);
+    }
+  }
 `;
 
 export default Wrapper;
