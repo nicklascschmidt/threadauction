8fd0edc1aef525e31ecd631dc5943a78d38b5d9b
Nicklas <nicklascschmidt@nicklass-mbp.home>
2018-11-07 00:23:18 -0800

    heroku hp updates

---
+++ client/build/precache-manifest.f2d8e268e03e496cb3de1cbe7c9ab275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e16f216119ff671cdb11",
+    "url": "/static/js/main.e16f2161.chunk.js"
+  },
+  {
+    "revision": "a3265913065e015f6cd6",
+    "url": "/static/js/1.a3265913.chunk.js"
+  },
+  {
+    "revision": "e16f216119ff671cdb11",
+    "url": "/static/css/main.250e8b82.chunk.css"
+  },
+  {
+    "revision": "fdfefa004fe0b71e488218cabb98c692",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b1bd9fbc72154136920c6a7784c422c.js"
+  "/precache-manifest.f2d8e268e03e496cb3de1cbe7c9ab275.js"
 );
 
 workbox.clientsClaim();
